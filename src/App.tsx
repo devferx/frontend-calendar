0fea45efaee2fcb8290b9ac41935f87851fa6b6f
devferx <ferkki@hotmail.com>
2021-10-31 14:18:49 -0400

    :recycle: [UPDATE] Calendar Title

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,22 @@
 import { Calendar } from "./components/Calendar";
-import { useCalendar } from "./hooks/useCalendar";
-import { generateArrayByDays } from "./utils/generateArrayByDays";
 import { getDayString } from "./utils/getDayString";
 import { getMothString } from "./utils/getMonthString";
 
 const App = () => {
-  const { month, year, nextMonth, prevMonth } = useCalendar();
   const now = new Date();
 
   return (
     <main className="main-container">
       <section className="container">
-        <div className="container__right">
-          <p className="container__right__title">{now.getDate()}</p>
-          <p className="container__right__desc">
+        <div className="container__left">
+          <p className="container__left__title">{now.getDate()}</p>
+          <p className="container__left__desc">
             {getDayString(now.getDay())} · {getMothString(now.getMonth())} ·{" "}
             {now.getFullYear()}
           </p>
         </div>
-        <div className="container__left">
-          <p className="container__left__title">{year}</p>
-          <Calendar
-            days={generateArrayByDays(month, year)}
-            year={year}
-            month={month}
-            prevMonth={prevMonth}
-            nextMonth={nextMonth}
-          />
-        </div>
+
+        <Calendar />
       </section>
     </main>
   );
--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,58 +1,51 @@
+import { useCalendar } from "../../hooks/useCalendar";
+
 import { isToday } from "../../utils/isToday";
 import { getMothString } from "../../utils/getMonthString";
+import { generateArrayByDays } from "../../utils/generateArrayByDays";
 
 import arrowLeft from "../../assets/arrow-left.svg";
 import arrowRight from "../../assets/arrow-right.svg";
 import "./calendar.css";
 
-interface CalendarProps {
-  days: number[];
-  month: number;
-  year: number;
-  nextMonth: () => void;
-  prevMonth: () => void;
-}
-
-export const Calendar = ({
-  days,
-  month,
-  year,
-  prevMonth,
-  nextMonth,
-}: CalendarProps) => {
+export const Calendar = () => {
+  const { month, year, prevMonth, nextMonth } = useCalendar();
   return (
-    <div className="calendar">
-      <div className="calendar__header">
-        <button className="calendar__header__button" onClick={prevMonth}>
-          <img src={arrowLeft} alt="Arrow Left" />
-        </button>
-        <span className="calendar__header__month">
-          {getMothString(month).toUpperCase()}
-        </span>
-        <button className="calendar__header__button" onClick={nextMonth}>
-          <img src={arrowRight} alt="Arrow Right" />
-        </button>
-      </div>
-      <div className="calendar__grid">
-        <span>DOM</span>
-        <span>LUN</span>
-        <span>MAR</span>
-        <span>MIÉ</span>
-        <span>JUE</span>
-        <span>VIE</span>
-        <span>SÁB</span>
-        {days.map((day, i) => (
-          <span
-            key={`${i}-${day}`}
-            className={
-              isToday(day, month, year)
-                ? "calendar__grid__item-active"
-                : undefined
-            }
-          >
-            {day != -1 && day}
+    <div className="container__right">
+      <p className="container__right__title">{year}</p>
+      <div className="calendar">
+        <div className="calendar__header">
+          <button className="calendar__header__button" onClick={prevMonth}>
+            <img src={arrowLeft} alt="Arrow Left" />
+          </button>
+          <span className="calendar__header__month">
+            {getMothString(month).toUpperCase()}
           </span>
-        ))}
+          <button className="calendar__header__button" onClick={nextMonth}>
+            <img src={arrowRight} alt="Arrow Right" />
+          </button>
+        </div>
+        <div className="calendar__grid">
+          <span>DOM</span>
+          <span>LUN</span>
+          <span>MAR</span>
+          <span>MIÉ</span>
+          <span>JUE</span>
+          <span>VIE</span>
+          <span>SÁB</span>
+          {generateArrayByDays(month, year).map((day, i) => (
+            <span
+              key={`${i}-${day}`}
+              className={
+                isToday(day, month, year)
+                  ? "calendar__grid__item-active"
+                  : undefined
+              }
+            >
+              {day != -1 && day}
+            </span>
+          ))}
+        </div>
       </div>
     </div>
   );
